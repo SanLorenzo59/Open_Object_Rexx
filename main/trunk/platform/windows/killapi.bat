89a490a9598409aca343ca21401bac25e70bb4a1
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2014-09-19 21:32:44 +0000

    Save a snapshot of the old trunk

--- main/trunk/platform/windows/killapi.bat
+++
@@ -1,4 +0,0 @@
-@REM Use taskkill to make sure that rxapi.exe
-@taskkill /F /IM rxapi.exe 2>NUL 1>NUL
-@REM force errorlevel to 0 to keep nmake from killing the build if this is not active.
-@EXIT /B 0
